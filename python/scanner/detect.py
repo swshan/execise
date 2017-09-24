b7b1f2d9c8619ca89b4639a65d114245c23f08d1
swsend <swcsend@gmail.com>
2017-09-24 15:17:06 +0800

    add forked scanner

---
+++ python/scanner/detect.py
@@ -0,0 +1,79 @@
+#coding=utf-8
+
+# by garson blog garnote.top
+import socket, ssl
+from netaddr import IPNetwork
+
+
+def detect (ip,timeout,hostname, port) :
+    timeout = 0.8
+    hostname
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(timeout)
+    try:
+        s.connect((ip, port))#219.76.4.4 218.254.1.13
+        s.close()
+        s.close()
+    except:
+        return False
+    return True
+
+#print(detect("219.76.4.4",2))
+
+def usage():
+    helps = '''
+-i, --in 读入ip文件路径。默认为/task.txt
+-o, --out 测试通过ip的保存路径。默认为/passip+输出时间.txt。
+-p, --parallels [number] 默认线程数为20。
+-t, --timeout [float] 超时时间 单位为s 默认为2s。
+-n, --hostname 默认为谷歌。
+-h, --help 帮助。
+    '''
+    print(helps)
+
+def gen_ip(a):
+    txt = a
+    temp = []
+    list1 = txt.split("\n")
+    for n in list1:
+        list2 = n.split('-')
+        if len(list2) == 2:
+            tmp = iprange(list2[0],list2[1])
+            for b in tmp:
+                temp.append(b)
+        elif len(n.split('/')) == 2:
+            tmp = IPNetwork(n)
+            for b in tmp:
+                temp.append(str(b))
+    return temp
+
+def ip2num(ip):#ip to int num
+    lp = [int(x) for x in ip.split('.')]
+    return lp[0] << 24 | lp[1] << 16 | lp[2] << 8 | lp[3]
+
+
+def num2ip(num):# int num to ip
+    ip = ['', '', '', '']
+    ip[3] = (num & 0xff)
+    ip[2] = (num & 0xff00) >> 8
+    ip[1] = (num & 0xff0000) >> 16
+    ip[0] = (num & 0xff000000) >> 24
+    return '%s.%s.%s.%s' % (ip[0], ip[1], ip[2], ip[3])
+
+
+def iprange(ip1,ip2):
+    num1 = ip2num(ip1)
+    num2 = ip2num(ip2)
+    temp = []
+    tmp = num2 - num1
+    n = 0
+    if tmp < 0:
+        return None
+    else:
+        for num in range(num1,num2 + 1):
+            temp.append(num2ip(num1 + n))
+            n +=1
+        return(temp)
+
+
+#print(iprange('192.168.199.1','192.168.200.1'))
---
+++ python/scanner/sni-detecter.py
@@ -0,0 +1,133 @@
+#coding=utf-8
+
+# origin auther: garsonbb
+# by garson blog garnote.top
+import sys, getopt
+import time
+from gevent.queue import Queue
+from gevent.threadpool import ThreadPool
+import gevent
+from gevent import monkey
+import requests
+
+from detect import *
+monkey.patch_all(thread=False, ssl=False, socket=True)
+
+pool = ThreadPool(3)
+q = Queue()
+
+rin = 'task.txt'
+output = 'replace'
+timeout = 1
+parallels = 20
+hostname = 'google.com'
+mod = True
+ips = []
+passip = []
+
+times = 0
+n = 1
+stop = False
+port = 1080
+
+
+def printx (text = '',type = 0):
+    global times ,n
+    p = int((times/n)*30)
+    t1 = '##############################' #30
+    t2 = '                              ' #30
+    if type == 1:
+        sys.stdout.write('                                                      \r')
+        sys.stdout.flush()
+        print(text)
+    else:
+        sys.stdout.write('[' + t1[0:p] + t2 [0:30-p] + ']' + '\r')
+        sys.stdout.flush()
+
+
+def worker(port):
+
+    while not q.empty():
+        global times ,n ,passip,stop
+        ip = q.get_nowait()
+        if detect(ip, timeout, hostname, port) and req_test(ip, port) == True:
+            passip.append(ip)
+            printx ('√   '+ip , 1)
+        times += 1
+        printx()
+
+        global output
+        #print ('√   finish  ' + 'This time seem'+ str(times) +' ip,'+'SNI_IP '+ str(len(passip)) +' s。')
+        print (times)
+        print (passip)
+
+def req_test(ip, port):
+    testUrl = 'http://www.douban.com'
+    timeout = 3
+    port = str(1080) if port is None else str(port)
+    proxies = {"http":"http://"+ip+":"+port,"https":"http://"+ip+":"+port
+    }
+    headers = {
+           'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0',
+           'Referer': 'http://www.zhihu.com/',
+           
+    }
+    try:
+        content=requests.get(testUrl,proxies=proxies,timeout=timeout, headers=headers).text.encode('UTF-8')
+    except Exception as e:
+        print (e)
+        print ("NetWork Error...")
+        return False
+    return True
+
+
+def caller(ips):
+    print 
+    requests = []
+    n = len(ips)
+    
+    port = 1080
+    for item in ips:
+        q.put(item)
+
+    gevent.joinall([
+    gevent.spawn(worker,port),
+    gevent.spawn(worker,port)
+    ])
+
+
+def main():
+    try:
+        opts ,args = getopt.getopt(sys.argv[1:],'i:o:t:p:n:h',['in','out','timeout','parallels','hostname'])
+    except getopt.GetoptError as err:
+        usage()
+        print(err)
+        sys.exit('parameter error')
+    global rin ,output ,timeout ,parallels ,ips ,mod ,hostname
+    for o, a in opts:
+        if o in ('-i','--in'):
+            rin = a
+        elif o in ('-o','--out'):
+            output = a
+        elif o in ('-h','--help'):
+            usage()
+            sys.exit()
+        elif o in ('-t','--timeout'):
+            timeout = int(a)
+        elif o in ('-n' , '--hostname'):
+            hostname = a
+
+    file_obj = open(rin)
+    try:
+        txt = file_obj.read()
+        ips = gen_ip(txt)
+        print('读入了'+ str(len(ips)) +'个ip')
+        caller(ips)
+    finally:
+        file_obj.close()
+
+if __name__ == '__main__':
+    main()
+
+
+
